ac7b3c5cc1220bb2d2156ad314408991ea71ce77
Liuba <pl@mail.ru>
2022-10-03 20:15:42 +0300

    Task 1

---
+++ ZD41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear(); 
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число и нажмите Enter: ");
+       int x = Convert.ToInt32(Console.ReadLine());
+       array[i] = x;
+    }
+
+}
+
+int ReleaseArray(int[] array) 
+{     int count = 0;     
+      for (int i = 0; i < array.Length; i++)     
+      {         if (array[i] > 0)             
+      count++;     
+      }     
+return count; } 
+
+
+Console.Write("Cколько чисел будем вводить? "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[m];
+Console.WriteLine(" "); 
+InputArray(array);
+
+Console.WriteLine(" "); 
+Console.Write("Чисел больше нуля: "); 
+Console.WriteLine(ReleaseArray(array));
